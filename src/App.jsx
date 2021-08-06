4c4e280c36c35f92d48e0530a6feb1a0d71aeb03
Matheus Rodrigues <matheusrf96@gmail.com>
2021-08-05 21:57:29 -0300

    Introduce flex box

--- src/App.jsx
+++ src/App.jsx
@@ -7,32 +7,36 @@ import Aleatorio from './components/basic/Aleatorio'
 
 import Card from './components/layout/Card'
 
+import './App.css'
+
 
 const App = () => {
     return (
         <div id="app">
             <h1>Fundamentos React</h1>
 
-            <Card title="01 - Primeiro componente">
-                <Primeiro />
-            </Card>
+            <div className="cards">
+                <Card title="01 - Primeiro componente">
+                    <Primeiro />
+                </Card>
 
-            <Card title="02 - Fragmento">
-                <Fragmento />
-            </Card>
+                <Card title="02 - Fragmento">
+                    <Fragmento />
+                </Card>
 
-            <Card title="03 - Componente com parâmetros">
-                <ComParametro titulo="Segundo componente" subtitulo="subtítulo teste" />
-            </Card>
+                <Card title="03 - Componente com parâmetros">
+                    <ComParametro titulo="Segundo componente" subtitulo="subtítulo teste" />
+                </Card>
 
-            <Card title="04 - Exemplo de Card">
-                <Aleatorio min={1} max={60} />
-                <Aleatorio min={1} max={60} />
-                <Aleatorio min={1} max={60} />
-                <Aleatorio min={1} max={60} />
-                <Aleatorio min={1} max={60} />
-                <Aleatorio min={1} max={60} />
-            </Card>
+                <Card title="04 - Exemplo de Card">
+                    <Aleatorio min={1} max={60} />
+                    <Aleatorio min={1} max={60} />
+                    <Aleatorio min={1} max={60} />
+                    <Aleatorio min={1} max={60} />
+                    <Aleatorio min={1} max={60} />
+                    <Aleatorio min={1} max={60} />
+                </Card>'
+            </div>
         </div>
     )
 }
