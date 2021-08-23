3d10e6d54ccb639d379ec39a288e9f40398280ca
Matheus Rodrigues <matheusrf96@gmail.com>
2021-08-23 20:04:04 -0300

    Implement Else component

--- src/components/conditional/If.js
+++ src/components/conditional/If.js
@@ -1,9 +1,23 @@
 const If = (props) => {
-    return (
-        props.test
-        ? props.children
-        : false
-    )
+    const elseChild = props.children.filter(child => {
+        return child.type && child.type.name === 'Else'
+    })[0]
+
+    const ifChildren = props.children.filter(child => {
+        return child !== elseChild
+    })
+
+    if (props.test) {
+        return ifChildren
+    }
+    else if (elseChild) {
+        return elseChild
+    }
+    else {
+        return false
+    }
 }
 
 export default If
+
+export const Else = (props) => props.children
--- src/components/conditional/UserInfo.jsx
+++ src/components/conditional/UserInfo.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
-import If from './If'
+import If, { Else } from './If'
 
 const UserInfo = (props) => {
     return (
         <div>
             <If test={ props.user && props.user.name }>
                 Seja bem-vindo, <strong>{ props.user.name }</strong>!
-            </If>
-
-            <If test={ !props.user || !props.user.name }>
-                Seja bem-vindo, <strong>camarada</strong>!
+                <Else>
+                    Seja bem-vindo, <strong>Camarada</strong>!
+                </Else>
             </If>
         </div>
     )
